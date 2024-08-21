9b042976da8c04e869805f1548391bc05bd8c7c8
Bismark Arroyo <bis.arroyo@gmail.com>
2024-08-20 21:35:27 -0600

    initialize spreadsheet state management and computation functions in main.js

--- main.js
+++ main.js
@@ -3,5 +3,79 @@ import './style.css'
 const $ = (id) => document.querySelector(id)
 const $$ = (id) => document.querySelectorAll(id)
 
-const ROWS = 100
-const COLUMNS = 100
+const ROWS = 10
+const COLUMNS = 10
+const FIRST_CHAR_CODE = 65
+
+const $table = $('table')
+const $thead = $('thead')
+const $tbody = $('tbody')
+
+const times = (length) => Array.from({ length }, (_, i) => i)
+
+const getColumn = (index) => String.fromCharCode(FIRST_CHAR_CODE + index)
+
+let selectedColumn = null
+let selectedRow = null
+
+let STATE = times(COLUMNS).map((i) =>
+  times(ROWS).map((j) => ({
+    computedValues: 0,
+    value: 0
+  }))
+)
+
+console.log(STATE)
+
+function updateCell(x, y, value) {
+  const newState = structuredClone(STATE)
+
+  const constants = generateCellsConstants(newState)
+
+  const cell = newState[x][y]
+}
+
+function generateCellsConstants(cells) {
+  return cells
+    .map((rows, x) => {
+      return rows
+        .map((cell, y) => {
+          const letter = getColumn(x) // -> A
+          const cellId = `${letter}${y + 1}` // -> A1
+          return `const ${cellId} = ${cell.computedValue};`
+        })
+        .join('\n')
+    })
+    .join('\n')
+}
+
+function computeAllCells(cells, constants) {
+  console.log('computeAllCells')
+  cells.forEach((rows, x) => {
+    rows.forEach((cell, y) => {
+      const computedValue = computeValue(cell.value, constants)
+      cell.computedValue = computedValue
+    })
+  })
+}
+
+function computeValue(value, constants) {
+  if (typeof value === 'number') return value
+  if (!value.startsWith('=')) return value
+
+  const formula = value.slice(1)
+
+  let computedValue
+  try {
+    computedValue = eval(`(() => {
+    ${constants}
+    return ${formula};
+  })()`)
+  } catch (e) {
+    computedValue = `!ERROR: ${e.message}`
+  }
+
+  console.log({ value, computedValue })
+
+  return computedValue
+}
